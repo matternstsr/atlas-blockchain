be132e116137b22fa2103475577e42e1c93ec558
matternstsr <matternstsr@gmail.com>
2025-01-21 12:02:02 -0600

    starting out

---
+++ crypto/ec_from_pub.c
@@ -0,0 +1,62 @@
+#include "hblk_crypto.h"
+
+/**
+* ec_from_pub - Creates an EC_KEY structure from a public key
+* @pub: The public key to convert
+*
+* Return: A pointer to the created EC_KEY structure, or NULL on failure
+*/
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+{
+	EC_KEY *key = NULL;
+	EC_GROUP *group = NULL;
+	EC_POINT *point = NULL;
+	BN_CTX *ctx = NULL;
+
+	if (!pub)
+		return (NULL);
+	/* Create a new EC_KEY structure using secp256k1 curve */
+	key = EC_KEY_new_by_curve_name(EC_CURVE);
+	if (!key)
+		return (NULL);
+	group = EC_KEY_get0_group(key);
+	if (!group)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	/* Create a new EC_POINT structure from the public key */
+	point = EC_POINT_new(group);
+	if (!point)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	ctx = BN_CTX_new();
+	if (!ctx)
+	{
+		EC_POINT_free(point);
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	/* Convert the public key (uncompressed format) to EC_POINT */
+	if (EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, ctx) != 1)
+	{
+		BN_CTX_free(ctx);
+		EC_POINT_free(point);
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	/* Set the public key of the EC_KEY structure */
+	if (EC_KEY_set_public_key(key, point) != 1)
+	{
+		BN_CTX_free(ctx);
+		EC_POINT_free(point);
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	/* Cleanup */
+	BN_CTX_free(ctx);
+	EC_POINT_free(point);
+	return (key);
+}
