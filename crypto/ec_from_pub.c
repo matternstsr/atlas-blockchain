f337bf1b1830da2f497e75a37993e60273d2ecaf
matternstsr <matternstsr@gmail.com>
2025-01-21 12:03:40 -0600

    starting out

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -9,7 +9,7 @@
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
 	EC_KEY *key = NULL;
-	EC_GROUP *group = NULL;
+	const EC_GROUP *group = NULL;  /* Change to const pointer */
 	EC_POINT *point = NULL;
 	BN_CTX *ctx = NULL;
 
