fb315ba98b3c8fec3cac1f090e1bf30342e5c501
matternstsr <matternstsr@gmail.com>
2025-01-21 12:23:40 -0600

    starting out

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,47 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <openssl/ec.h>
 #include <openssl/pem.h>
 #include <openssl/err.h>
-#include "hblk_crypto.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 EC_KEY *ec_load(char const *folder)
 {
-	char priv_key_path[256];
-	char pub_key_path[256];
-	EC_KEY *key = NULL;
-	FILE *priv_file = NULL, *pub_file = NULL;
+    char priv_key_path[512], pub_key_path[512];
+    FILE *priv_key_file = NULL, *pub_key_file = NULL;
+    EC_KEY *ec_key = NULL;
 
-	snprintf(priv_key_path, sizeof(priv_key_path), "%s/key.pem", folder);
-	snprintf(pub_key_path, sizeof(pub_key_path), "%s/key_pub.pem", folder);
+    /* Construct the file paths */
+    snprintf(priv_key_path, sizeof(priv_key_path), "%s/key.pem", folder);
+    snprintf(pub_key_path, sizeof(pub_key_path), "%s/key_pub.pem", folder);
 
-	priv_file = fopen(priv_key_path, "r");
-	if (!priv_file)
-	{
-		perror("Failed to open private key file");
-		return NULL;
-	}
-	key = PEM_read_ECPrivateKey(priv_file, NULL, NULL, NULL);
-	fclose(priv_file);
-	if (!key)
-	{
-		fprintf(stderr, "Failed to load private key from %s\n", priv_key_path);
-		return NULL;
-	}
-	pub_file = fopen(pub_key_path, "r");
-	if (!pub_file)
-	{
-		fprintf(stderr, "Failed to open public key file\n");
-		EC_KEY_free(key);
-		return NULL;
-	}
-	if (!PEM_read_ECPublicKey(pub_file, &key, NULL, NULL))
-	{
-		fprintf(stderr, "Failed to load public key from %s\n", pub_key_path);
-		fclose(pub_file);
-		EC_KEY_free(key);
-		return NULL;
-	}
-	fclose(pub_file);
-	return key;
+    /* Load the private key */
+    priv_key_file = fopen(priv_key_path, "r");
+    if (!priv_key_file)
+    {
+        fprintf(stderr, "Error: Unable to open private key file: %s\n", priv_key_path);
+        return NULL;
+    }
+
+    ec_key = PEM_read_ECPrivateKey(priv_key_file, NULL, NULL, NULL);
+    fclose(priv_key_file);
+    if (!ec_key)
+    {
+        fprintf(stderr, "Error: Unable to load private key from %s\n", priv_key_path);
+        return NULL;
+    }
+
+    /* Load the public key */
+    pub_key_file = fopen(pub_key_path, "r");
+    if (!pub_key_file)
+    {
+        fprintf(stderr, "Error: Unable to open public key file: %s\n", pub_key_path);
+        EC_KEY_free(ec_key);
+        return NULL;
+    }
+
+    if (!PEM_read_ECPublicKey(pub_key_file, &ec_key, NULL, NULL))
+    {
+        fprintf(stderr, "Error: Unable to load public key from %s\n", pub_key_path);
+        fclose(pub_key_file);
+        EC_KEY_free(ec_key);
+        return NULL;
+    }
+
+    fclose(pub_key_file);
+
+    /* Success */
+    return ec_key;
 }
