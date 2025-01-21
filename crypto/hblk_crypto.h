fd5a13b3e582ca651b3bee21493d2c60031d0628
matternstsr <matternstsr@gmail.com>
2025-01-21 12:13:58 -0600

    starting out

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -17,7 +17,4 @@ int ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 
-/* Utility Functions */
-/* static void _print_hex_buffer(uint8_t const *buf, size_t len); */
-
 #endif /* HBLK_CRYPTO_H */
