b7068a08c329939ea16bcce33d761de61be8ea2a
matternstsr <matternstsr@gmail.com>
2025-01-21 11:54:36 -0600

    starting out

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -1,15 +1,6 @@
-/* ec_to_pub.c */
 #include "hblk_crypto.h"
 
-/**
-* ec_to_pub - Extracts the public key from an EC_KEY structure.
-* @key: Pointer to the EC_KEY structure.
-* @pub: Buffer to store the extracted public key.
-*
-* Return: A pointer to @pub, or NULL on failure.
-*/
-uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
-{
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]) {
 	const EC_GROUP *group;
 	const EC_POINT *point;
 	BN_CTX *ctx = NULL;
@@ -27,8 +18,7 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 		return (NULL);
 
 	if (EC_POINT_point2oct(group, point, POINT_CONVERSION_UNCOMPRESSED,
-						pub, EC_PUB_LEN, ctx) == 0)
-	{
+						pub, EC_PUB_LEN, ctx) == 0) {
 		BN_CTX_free(ctx);
 		return (NULL);
 	}
