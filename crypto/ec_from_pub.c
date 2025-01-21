256fbcd553bbff70810fcf203a688ae55c5644d6
matternstsr <matternstsr@gmail.com>
2025-01-21 11:50:57 -0600

    starting out

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -1,3 +1,4 @@
+/* ec_to_pub.c */
 #include "hblk_crypto.h"
 
 /**
@@ -15,19 +16,23 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 
 	if (!key || !pub)
 		return (NULL);
+
 	group = EC_KEY_get0_group(key);
 	point = EC_KEY_get0_public_key(key);
 	if (!group || !point)
 		return (NULL);
+
 	ctx = BN_CTX_new();
 	if (!ctx)
 		return (NULL);
+
 	if (EC_POINT_point2oct(group, point, POINT_CONVERSION_UNCOMPRESSED,
 						pub, EC_PUB_LEN, ctx) == 0)
 	{
 		BN_CTX_free(ctx);
 		return (NULL);
 	}
+
 	BN_CTX_free(ctx);
 	return (pub);
 }
