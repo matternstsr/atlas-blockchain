1729101983ff7fecee5ef7c5b62a9f3ff911de7a
matternstsr <matternstsr@gmail.com>
2025-01-23 11:00:00 -0600

    starting out

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -19,4 +19,5 @@ EC_KEY *ec_create(void);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+EC_KEY *ec_load(char const *folder);
 #endif /* HBLK_CRYPTO_H */
