51fc3f8e181ee1ba8a70f4a3c4d8c11cd0f76bd2
matternstsr <matternstsr@gmail.com>
2025-01-23 10:38:33 -0600

    starting out

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -13,9 +13,8 @@
 
 /* EC Key Pair Functions */
 EC_KEY *ec_create(void);
-/* int ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);*/
-uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
+int ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
-EC_KEY *ec_load(char const *folder);
+
 #endif /* HBLK_CRYPTO_H */
