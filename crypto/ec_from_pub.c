ab421a167c870c4206891347bfccea829ab2b276
matternstsr <matternstsr@gmail.com>
2025-01-29 13:01:00 -0600

    cleaning

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -9,13 +9,12 @@
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
 	EC_KEY *key = NULL;
-	const EC_GROUP *group = NULL;  /* Change to const pointer */
+	const EC_GROUP *group = NULL;
 	EC_POINT *point = NULL;
 	BN_CTX *ctx = NULL;
 
 	if (!pub)
-		return (NULL);
-	/* Create a new EC_KEY structure using secp256k1 curve */
+		return (NULL); /* Create a new EC_KEY struct using secp256k1 curve */
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	if (!key)
 		return (NULL);
@@ -24,8 +23,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	{
 		EC_KEY_free(key);
 		return (NULL);
-	}
-	/* Create a new EC_POINT structure from the public key */
+	}/* Create a new EC_POINT structure from the public key */
 	point = EC_POINT_new(group);
 	if (!point)
 	{
@@ -38,24 +36,21 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 		EC_POINT_free(point);
 		EC_KEY_free(key);
 		return (NULL);
-	}
-	/* Convert the public key (uncompressed format) to EC_POINT */
+	}/* Convert the public key (uncompressed format) to EC_POINT */
 	if (EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, ctx) != 1)
 	{
 		BN_CTX_free(ctx);
 		EC_POINT_free(point);
 		EC_KEY_free(key);
 		return (NULL);
-	}
-	/* Set the public key of the EC_KEY structure */
+	}/* Set the public key of the EC_KEY structure */
 	if (EC_KEY_set_public_key(key, point) != 1)
 	{
 		BN_CTX_free(ctx);
 		EC_POINT_free(point);
 		EC_KEY_free(key);
 		return (NULL);
-	}
-	/* Cleanup */
+	}/* Cleanup */
 	BN_CTX_free(ctx);
 	EC_POINT_free(point);
 	return (key);
