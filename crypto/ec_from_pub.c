6f00e502223987c222be07ff486c5f2adee63812
matternstsr <matternstsr@gmail.com>
2025-01-29 13:06:03 -0600

    cleaning

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -1,34 +1,34 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_from_pub - Creates an EC_KEY structure from a public key
- * @pub: The public key to convert
- *
- * Return: A pointer to the created EC_KEY structure, or NULL on failure
- */
+* ec_from_pub - Creates an EC_KEY structure from a public key
+* @pub: The public key to convert
+*
+* Return: A pointer to the created EC_KEY structure, or NULL on failure
+*/
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
-    EC_KEY *key = NULL;
-    EC_POINT *point = NULL;
-    BN_CTX *ctx = NULL;
-    const EC_GROUP *group;
+	EC_KEY *key = NULL;
+	EC_POINT *point = NULL;
+	BN_CTX *ctx = NULL;
+	const EC_GROUP *group;
 
-    if (!pub || !(key = EC_KEY_new_by_curve_name(EC_CURVE)) ||
+	if (!pub || !(key = EC_KEY_new_by_curve_name(EC_CURVE)) ||
 			!(group = EC_KEY_get0_group(key)) ||
 			!(point = EC_POINT_new(group)) || !(ctx = BN_CTX_new()))
-        goto cleanup;
+		goto cleanup;
 
-    if (EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, ctx) != 1 ||
+	if (EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, ctx) != 1 ||
 							EC_KEY_set_public_key(key, point) != 1)
-        goto cleanup;
+		goto cleanup;
 
-    BN_CTX_free(ctx);
-    EC_POINT_free(point);
-    return (key);
+	BN_CTX_free(ctx);
+	EC_POINT_free(point);
+	return (key);
 
 cleanup:
-    EC_KEY_free(key);
-    EC_POINT_free(point);
-    BN_CTX_free(ctx);
-    return (NULL);
+	EC_KEY_free(key);
+	EC_POINT_free(point);
+	BN_CTX_free(ctx);
+	return (NULL);
 }
