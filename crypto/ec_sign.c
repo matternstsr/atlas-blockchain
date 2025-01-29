dd9232eea7f8e112a4d741f0a2a76dfaa64e9b2e
matternstsr <matternstsr@gmail.com>
2025-01-29 14:03:40 -0600

    cleaning

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,35 +1,25 @@
 #include "hblk_crypto.h"
 
 /**
-* ec_sign - Signs a message using a given EC_KEY private key
-* @key: Pointer to the EC_KEY structure containing the private key
-* @msg: Pointer to the message to be signed
-* @msglen: Length of the message
-* @sig: Pointer to sig_t struct to store the signature
-* Return: Pointer to signature buffer on success, NULL on failure
+* ec_sign - Signs the given EC_KEY
+* @key: key to be signed
+* @msg: characters to be signed
+* @msglen: number of characters to sign
+* @sig: address to hold signiture
+* Return: pointer to sig or NULL
 */
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-					sig_t *sig)
+				sig_t *sig)
 {
-	unsigned int sig_len;
-
-	if (!key || !msg || !sig)
+	if (!key || !msg)
 		return (NULL);
 
-	sig->len = ECDSA_size(key);
-	if (!sig->sig)
+	if (!ECDSA_sign(0, msg, msglen, sig->sig, (unsigned int *)&sig->len,
+					(EC_KEY *)key))
 	{
-		sig->sig = malloc(sig->len);
-		if (!sig->sig)
-			return (NULL); /* Return NULL if malloc fails */
-		memset(sig->sig, 0, sig->len); /* Clear the memory with correct size */
-	}
-	if (!ECDSA_sign(0, msg, msglen, sig->sig, &sig_len, (EC_KEY *)key))
-	{
-		free(sig->sig);
-		sig->sig = NULL; /* Ensure we reset the pointer on failure */
+		sig->len = 0;
 		return (NULL);
 	}
-	sig->len = sig_len;
+
 	return (sig->sig);
 }
