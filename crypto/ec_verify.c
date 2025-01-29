99c5a1a0f6771d2f87a5b7d0bae2bb8cc5aa3378
matternstsr <matternstsr@gmail.com>
2025-01-29 13:59:22 -0600

    cleaning

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -10,10 +10,10 @@
 * Return: 1 if signature is valid, 0 otherwise
 */
 int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-				sig_t const *sig)
+			  sig_t const *sig)
 {
-	if (!key || !msg || !sig || !sig->sig || sig->len == 0)
+	if (!key || !msg || !sig || !sig->len)
 		return (0);
 
-	return ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key) == 1;
+	return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key));
 }
