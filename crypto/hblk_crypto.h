602f50959d0f84f9ed9f62687d997e5cb55ed53e
matternstsr <matternstsr@gmail.com>
2025-01-29 13:32:18 -0600

    cleaning

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -23,9 +23,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 uint8_t *sha256(int8_t const *s, size_t len,
 				uint8_t digest[SHA256_DIGEST_LENGTH]);
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-					sig_t *sig);
-int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-				sig_t const *sig);
+int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig);
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig);
 EC_KEY *ec_load(char const *folder);
 #endif /* HBLK_CRYPTO_H */
