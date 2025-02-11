86bf650c6eb65220432ab3e2b3266be01d522140
Matt <matternstsr@gmail.com>
2025-02-11 13:03:57 -0600

    trying the SHA256 function directly again but not sure i get this totally

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,39 +1,28 @@
 #include "blockchain.h"
 
 /**
- * block_hash - computes the hash of a block
- * @block: pointer to the block whose hash is to be computed
- * @hash_buf: buffer to store the computed hash
- * Return: pointer to the hash_buf containing the computed hash
+ * block_hash - hashes a block using sha256
+ * @block: block to hash
+ * @hash_buf: buffer to store computed hash
+ * Return: hash buffer or NULL
  */
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+uint8_t *block_hash(block_t const *block,
+					uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	unsigned char data[256];   /* Buffer to hold the serialized data */
-	size_t data_len = 0;       /* Data length to keep track of the total size */
+	size_t data_len;
 
-	/* Serialize block info fields */
-	memcpy(data + data_len, &block->info.index, sizeof(block->info.index));
-	data_len += sizeof(block->info.index);
-	/* Use the hardcoded timestamp */
-	uint32_t timestamp = 1537578000;  /* Hardcoded timestamp */
-	memcpy(data + data_len, &timestamp, sizeof(timestamp));
-	data_len += sizeof(timestamp);
-	memcpy(data + data_len, &block->info.difficulty,
-			sizeof(block->info.difficulty));
-	data_len += sizeof(block->info.difficulty);
-	memcpy(data + data_len, &block->info.nonce, sizeof(block->info.nonce));
-	data_len += sizeof(block->info.nonce);
-	memcpy(data + data_len, block->info.prev_hash, SHA256_DIGEST_LENGTH);
-	data_len += SHA256_DIGEST_LENGTH;
-	/* Append block data: its length and its content */
-	memcpy(data + data_len, &block->data.len, sizeof(block->data.len));
-	data_len += sizeof(block->data.len);
+	if (!block || !hash_buf)
+		return (NULL);
+	/* Serialize the block: info + data */
+	unsigned char data[sizeof(block->info) + block->data.len];
+	data_len = 0;
+	/* Copy block's info */
+	memcpy(data + data_len, &block->info, sizeof(block->info));
+	data_len += sizeof(block->info);
+	/* Copy block's data */
 	memcpy(data + data_len, block->data.buffer, block->data.len);
 	data_len += block->data.len;
-	/* Compute the SHA-256 hash of the serialized data */
-	SHA256_CTX sha256_ctx;
-	SHA256_Init(&sha256_ctx);
-	SHA256_Update(&sha256_ctx, data, data_len);
-	SHA256_Final(hash_buf, &sha256_ctx);
+	/* Hash the serialized data */
+	SHA256(data, data_len, hash_buf);
 	return hash_buf;
 }
