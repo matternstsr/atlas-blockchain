f4084fda4cd31a098a613707cfa994050885dd9a
Matt <matternstsr@gmail.com>
2025-02-11 10:48:49 -0600

    cleanup

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -1,21 +1,21 @@
 #include "blockchain.h"
 
 /**
- * blockchain_destroy - frees the memory allocated for a blockchain
+ * blockchain_destroy - Frees the memory allocated for a blockchain
  * @blockchain: pointer to the blockchain structure to be freed
  */
 void blockchain_destroy(blockchain_t *blockchain)
 {
 	block_t *block;
-	if (blockchain)
+	if (blockchain) 
 	{
 		/* Free each block in the linked list */
 		while ((block = llist_pop(blockchain->chain)) != NULL)
-		{
-			block_destroy(block);  
+			block_destroy(block);
 			/* Use the block_destroy function to free each block */
-		}
-		/* Free the blockchain structure */
+		/* Free the linked list structure */
+		llist_destroy(blockchain->chain, 1, NULL);
+		/* Finally, free the blockchain structure */
 		free(blockchain);
 	}
 }
