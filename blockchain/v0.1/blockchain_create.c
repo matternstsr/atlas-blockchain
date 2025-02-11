8c18498d88603ea9c3f2f79763ad07dc54c05cac
Matt <matternstsr@gmail.com>
2025-02-11 10:26:33 -0600

    think this is it...

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -6,41 +6,40 @@
  */
 blockchain_t *blockchain_create(void)
 {
-	blockchain_t *new_chain = NULL;
-	block_t *new_block = NULL;
-
+	blockchain_t *chain = NULL; /* Changed name from new_chain */
+	block_t *genesis_block = NULL; /* Changed name from new_block */
 	/* Initialize block_info and block_data structures */
-	block_info_t info = {0, 0, 1537578000, 0, {0}};
-	block_data_t data = {"Holberton School", 16};
+	block_info_t block_info = {0, 0, 1537578000, 0, {0}}; /* Changed from info */
+	block_data_t block_data = {"Holberton School", 16}; /* Changed from data */
 	/* Allocate memory for blockchain */
-	new_chain = malloc(sizeof(blockchain_t));
-	if (!new_chain)
+	chain = malloc(sizeof(blockchain_t));
+	if (!chain)
 		return (NULL);
 	/* Allocate memory for the genesis block */
-	new_block = malloc(sizeof(block_t));
-	if (!new_block)
-		return (free(new_chain), NULL);
+	genesis_block = malloc(sizeof(block_t));
+	if (!genesis_block)
+		return (free(chain), NULL);
 	/* Create a new linked list for the blockchain */
-	new_chain->chain = llist_create(MT_SUPPORT_FALSE);
-	if (!new_chain->chain)
+	chain->chain = llist_create(MT_SUPPORT_FALSE);
+	if (!chain->chain)
 	{
-		free(new_block);
-		free(new_chain);
+		free(genesis_block);
+		free(chain);
 		return (NULL);
 	}
 	/* Set the info and data of the genesis block */
-	new_block->info = info;
-	new_block->data = data;
+	genesis_block->info = block_info;
+	genesis_block->data = block_data;
 	/* Assign predefined hash for the genesis block */
-	memcpy(new_block->hash, HOLBERTON_HASH, SHA256_DIGEST_LENGTH);
+	memcpy(genesis_block->hash, HOLBERTON_HASH, SHA256_DIGEST_LENGTH);
 	/* Add the genesis block to the chain */
-	if (llist_add_node(new_chain->chain, new_block, ADD_NODE_REAR) == -1)
+	if (llist_add_node(chain->chain, genesis_block, ADD_NODE_REAR) == -1)
 	{
-		llist_destroy(new_chain->chain, 0, NULL);
-		free(new_chain);
-		free(new_block);
+		llist_destroy(chain->chain, 0, NULL);
+		free(chain);
+		free(genesis_block);
 		return (NULL);
 	}
 	/* Return the created blockchain */
-	return (new_chain);
+	return (chain);
 }
