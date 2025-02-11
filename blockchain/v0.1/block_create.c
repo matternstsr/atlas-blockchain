affd49dd06b05482c08b297938c4a28d47d281fc
Matt <matternstsr@gmail.com>
2025-02-11 10:22:23 -0600

    think this is it...

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -1,40 +1,44 @@
 #include "blockchain.h"
 
 /**
- * block_create - creates a new Block structure and initializes it
- * @prev: The previous block in the blockchain
- * @data: Data to be stored in the block
- * @data_len: The length of the data to duplicate
- * Return: A pointer to the newly created block
- * Definition: The difficulty is a measure of how hard it is to find a
- *  valid block hash that meets certain criteria. In proof-of-work systems,
- *  miners must find a hash that is less than a target value.
- * Definition: The nonce is a number that miners adjust during the mining
- *  process in order to change the resulting hash of a block. The nonce is
- *  part of the block header, and miners increment it in order to find a
- *  valid hash.
+ * block_create - Creates a new block
+ * @prev: Previous block
+ * @data: data to duplicate into block
+ * @data_len: Number of bytes to copy
+ * Return: Pointer to new block or NULL
  */
 block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len)
 {
-	block_t *new_block = NULL;
+    block_t *new_block = NULL;
+    block_info_t info;
+    block_data_t new_data = {{0}, 0};
+    uint32_t len = data_len;
 
-	/*  memory for the new block */
-	new_block = malloc(sizeof(block_t));
-	if (!new_block)
-		return (NULL);
-	/* Set the index: previous block's index + 1 */
-	new_block->index = prev->index + 1;
-	/* Set difficulty and nonce to 0 */
-	new_block->difficulty = 0;
-	new_block->nonce = 0;
-	/* Set the timestamp using the current time */
-	new_block->timestamp = time(NULL);
-	/* Copy the previous block's hash */
-	memcpy(new_block->prev_hash, prev->hash, sizeof(new_block->prev_hash));
-	/* Copy the data, ensuring we do not exceed the maximum allowed length */
-	new_block->data.len = (data_len > BLOCKCHAIN_DATA_MAX) ? BLOCKCHAIN_DATA_MAX : data_len;
-	memcpy(new_block->data.buffer, data, new_block->data.len);
-	/* Set the hash to all zeroes (we will hash it later) */
-	memset(new_block->hash, 0, sizeof(new_block->hash));
-	return (new_block);
+    new_block = malloc(sizeof(block_t));
+    if (!new_block)
+        return (NULL);
+
+    /* Ensure data length doesn't exceed max */
+    if (data_len > BLOCKCHAIN_DATA_MAX)
+        len = BLOCKCHAIN_DATA_MAX;
+
+    /* Copy the data into the new block's buffer */
+    memcpy(new_data.buffer, data, len);
+    new_data.len = len;
+
+    /* Set up the block information (index, timestamp, prev_hash) */
+    info.index = prev->info.index + 1;          /* Next block index */
+    info.difficulty = 0;                         /* Difficulty (could be used later) */
+    info.nonce = 0;                              /* Nonce (for proof of work) */
+    memcpy(info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH); /* Copy the previous block's hash */
+    info.timestamp = time(NULL);                 /* Current timestamp for block creation */
+
+    /* Initialize the hash to zero initially (to be computed later) */
+    memset(new_block->hash, 0, SHA256_DIGEST_LENGTH);
+
+    /* Set the new block's data and info */
+    new_block->data = new_data;
+    new_block->info = info;
+
+    return (new_block);
 }
--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,44 +1,46 @@
 #include "blockchain.h"
 
 /**
- * block_create - Creates a new block
- * @prev: Previous block
- * @data: data to duplicate into block
- * @data_len: Number of bytes to copy
- * Return: Pointer to new block or NULL
+ * blockchain_create - Creates a new blockchain
+ * Return: Pointer to new chain or NULL
  */
-block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len)
+blockchain_t *blockchain_create(void)
 {
-    block_t *new_block = NULL;
-    block_info_t info;
-    block_data_t new_data = {{0}, 0};
-    uint32_t len = data_len;
+	blockchain_t *new_chain = NULL;
+	block_t *new_block = NULL;
 
-    new_block = malloc(sizeof(block_t));
-    if (!new_block)
-        return (NULL);
-
-    /* Ensure data length doesn't exceed max */
-    if (data_len > BLOCKCHAIN_DATA_MAX)
-        len = BLOCKCHAIN_DATA_MAX;
-
-    /* Copy the data into the new block's buffer */
-    memcpy(new_data.buffer, data, len);
-    new_data.len = len;
-
-    /* Set up the block information (index, timestamp, prev_hash) */
-    info.index = prev->info.index + 1;          /* Next block index */
-    info.difficulty = 0;                         /* Difficulty (could be used later) */
-    info.nonce = 0;                              /* Nonce (for proof of work) */
-    memcpy(info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH); /* Copy the previous block's hash */
-    info.timestamp = time(NULL);                 /* Current timestamp for block creation */
-
-    /* Initialize the hash to zero initially (to be computed later) */
-    memset(new_block->hash, 0, SHA256_DIGEST_LENGTH);
-
-    /* Set the new block's data and info */
-    new_block->data = new_data;
-    new_block->info = info;
-
-    return (new_block);
+	/* Initialize block_info and block_data structures */
+	block_info_t info = {0, 0, 1537578000, 0, {0}};
+	block_data_t data = {"Holberton School", 16};
+	/* Allocate memory for blockchain */
+	new_chain = malloc(sizeof(blockchain_t));
+	if (!new_chain)
+		return (NULL);
+	/* Allocate memory for the genesis block */
+	new_block = malloc(sizeof(block_t));
+	if (!new_block)
+		return (free(new_chain), NULL);
+	/* Create a new linked list for the blockchain */
+	new_chain->chain = llist_create(MT_SUPPORT_FALSE);
+	if (!new_chain->chain)
+	{
+		free(new_block);
+		free(new_chain);
+		return (NULL);
+	}
+	/* Set the info and data of the genesis block */
+	new_block->info = info;
+	new_block->data = data;
+	/* Assign predefined hash for the genesis block */
+	memcpy(new_block->hash, HOLBERTON_HASH, SHA256_DIGEST_LENGTH);
+	/* Add the genesis block to the chain */
+	if (llist_add_node(new_chain->chain, new_block, ADD_NODE_REAR) == -1)
+	{
+		llist_destroy(new_chain->chain, 0, NULL);
+		free(new_chain);
+		free(new_block);
+		return (NULL);
+	}
+	/* Return the created blockchain */
+	return (new_chain);
 }
