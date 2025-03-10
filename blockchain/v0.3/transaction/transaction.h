00de396585361205dfb9ac049268f24fe8a9483b
matternstsr <matternstsr@gmail.com>
2025-03-10 18:14:19 -0500

    nothing works so started over

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -56,6 +56,20 @@ typedef struct transaction_s {
     size_t output_count;    /* Number of outputs */
 } transaction_t;
 
+/**
+ * struct tx_out_s - Transaction output
+ *
+ * @amount: Amount received
+ * @pub:    Receiver's public address
+ * @hash:   Hash of @amount and @pub. Serves as output ID
+ */
+typedef struct tx_out_s
+{
+	uint32_t    amount;
+	uint8_t     pub[EC_PUB_LEN];
+	uint8_t     hash[SHA256_DIGEST_LENGTH];
+} tx_out_t, to_t;
+
 /* Function Prototypes */
 tx_out_t *tx_out_create(uint32_t amount, const uint8_t pub[EC_PUB_LEN]);
 
