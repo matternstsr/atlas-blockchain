fb7f4206473e014154d09999b0d4a843e5a11b7c
matternstsr <matternstsr@gmail.com>
2025-03-10 19:19:08 -0500

    nothing works so started over

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -1,23 +1,30 @@
 #include "transaction.h"
 
 /**
- * tx_out_create - Creates a transaction out struct
- * @pub: public key
- * @amount: amount of transaction
- * Return: pointer to new struct or NULL
- */
-to_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
+* tx_out_create - Creates a new transaction output struct.
+* @pub: Public key associated with the transaction output.
+* @amount: Amount of the transaction output.
+* Return: Pointer to the new struct or NULL in case of failure.
+*/
+to_t *tx_out_create(uint32_t amount, const uint8_t pub[EC_PUB_LEN])
 {
-	to_t *new;
+	to_t *new_output = NULL;
 
-	if (!pub || !amount)
-		return (NULL);
+	/* Check if public key or amount is invalid */
+	if (!pub || amount == 0)
+		return NULL;
 
-	new = calloc(1, sizeof(to_t));
-	if (!new)
-		return (NULL);
-	new->amount = amount;
-	memcpy(new->pub, pub, EC_PUB_LEN);
-	SHA256((uint8_t *)new, (EC_PUB_LEN + sizeof(uint32_t)), new->hash);
-	return (new);
+	/* Allocate memory for the new transaction output struct */
+	new_output = calloc(1, sizeof(to_t));
+	if (!new_output)
+		return NULL;
+
+	/* Assign the transaction output values */
+	new_output->amount = amount;
+	memcpy(new_output->pub, pub, EC_PUB_LEN);
+
+	/* Compute the hash for the transaction output */
+	SHA256((uint8_t *)new_output, sizeof(uint32_t) + EC_PUB_LEN, new_output->hash);
+
+	return new_output;
 }
