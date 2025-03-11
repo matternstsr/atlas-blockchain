d9bbe70d14a2ab3222094b7196f7558f6ab97792
matternstsr <matternstsr@gmail.com>
2025-03-11 16:16:34 -0500

    fixing up coinbase

---
+++ blockchain/v0.3/transaction/transaction_destroy.c
@@ -0,0 +1,27 @@
+#include "transaction.h"
+
+/**
+ * transaction_destroy - Frees a transaction and its associated lists
+ * @transaction: The transaction to be freed
+ */
+void transaction_destroy(transaction_t *transaction)
+{
+    /* Ensure transaction is not NULL */
+    if (!transaction)
+        return;
+
+    /* Destroy the outputs list */
+    if (llist_size(transaction->outputs) > 0)
+        llist_destroy(transaction->outputs, 1, NULL);
+    else
+        llist_destroy(transaction->outputs, 0, NULL);
+
+    /* Destroy the inputs list */
+    if (llist_size(transaction->inputs) > 0)
+        llist_destroy(transaction->inputs, 1, NULL);
+    else
+        llist_destroy(transaction->inputs, 0, NULL);
+
+    /* Free the transaction itself */
+    free(transaction);
+}
