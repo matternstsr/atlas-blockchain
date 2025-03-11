f38b76f1fc80ecbad1ba622b14ea8ba010f15501
matternstsr <matternstsr@gmail.com>
2025-03-10 19:21:37 -0500

    nothing works so started over

---
+++ blockchain/v0.3/transaction/transaction.h
@@ -0,0 +1,29 @@
+#ifndef _TRANSACTION_H_
+#define _TRANSACTION_H_
+
+#include <openssl/sha.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Structs */
+
+/**
+* struct tx_out_s - Transaction output
+*
+* @amount: Amount received
+* @pub:    Receiver's public address
+* @hash:   Hash of @amount and @pub. Serves as output ID
+*/
+typedef struct tx_out_s
+{
+	uint32_t    amount;
+	uint8_t     pub[EC_PUB_LEN];
+	uint8_t     hash[SHA256_DIGEST_LENGTH];
+} tx_out_t, to_t;
+
+/* Prototypes */
+
+to_t *tx_out_create(uint32_t amount, const uint8_t pub[EC_PUB_LEN]);
+
+#endif
