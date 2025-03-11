b1068910d2eb717af45d7998226d6183102aa590
matternstsr <matternstsr@gmail.com>
2025-03-10 19:09:26 -0500

    nothing works so started over

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -6,6 +6,13 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include "provided/endianness.h"
+#include "transaction/transaction.h"
+#include <llist.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
 
 /* Structs */
 
