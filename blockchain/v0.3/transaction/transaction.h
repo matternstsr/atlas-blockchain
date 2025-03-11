1ded822bffa7e8412f2d10badc9ff6892acdd8a8
matternstsr <matternstsr@gmail.com>
2025-03-10 19:27:36 -0500

    nothing works so started over

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -12,14 +12,38 @@
 #include <string.h>
 #include <time.h>
 
+/* Macros */
+#define COINBASE_AMOUNT 50
+#define BLOCKCHAIN_DATA_MAX 1024
+#define BUFF_SIZE ((32 * 3 * ins) + (32 * outs))
+#define PTR_MOVE (sizeof(uint32_t) + EC_PUB_LEN)
+#define UNSPENT ((uto_t *)unspent)
+#define CONTEXT ((tc_t *)context)
+
+
 /* Structs */
 
 /**
-* struct tx_out_s - Transaction output
+* struct transaction_s - Represents a transaction
 *
-* @amount: Amount received
-* @pub:    Receiver's public address
-* @hash:   Hash of @amount and @pub. Serves as output ID
+* @id:      Transaction identifier, a hash of all the inputs and outputs.
+*           This ensures the integrity of the transaction, preventing further modification.
+* @inputs:  List of `tx_in_t *` structures. Represents the transaction's inputs.
+* @outputs: List of `tx_out_t *` structures. Represents the transaction's outputs.
+*/
+typedef struct transaction_s
+{
+	uint8_t     id[SHA256_DIGEST_LENGTH];
+	llist_t     *inputs;
+	llist_t     *outputs;
+} transaction_t;
+
+/**
+* struct tx_out_s - Represents a transaction output
+*
+* @amount: The amount being transferred to the recipient
+* @pub:    The public key address of the recipient
+* @hash:   A hash of @amount and @pub, used as the unique identifier for the output
 */
 typedef struct tx_out_s
 {
@@ -29,22 +53,93 @@ typedef struct tx_out_s
 } tx_out_t, to_t;
 
 /**
- * struct transaction_s - Transaction structure
- *
- * @id:      Transaction ID. A hash of all the inputs and outputs.
- *           Prevents further alteration of the transaction.
- * @inputs:  List of `tx_in_t *`. Transaction inputs
- * @outputs: List of `tx_out_t *`. Transaction outputs
- */
-typedef struct transaction_s
+* struct tx_in_s - Represents a transaction input
+*
+* Description: A transaction input typically references a previous transaction output,
+* unless it is a Coinbase transaction, which introduces new coins into circulation.
+*
+* @block_hash:  The hash of the block that contains the transaction identified by @tx_id
+* @tx_id:       The ID of the transaction containing the referenced output
+* @tx_out_hash: The hash of the specific transaction output being referenced
+* @sig:         A signature that prevents any alterations to the transaction's content.
+*               The input is signed by the recipient of the referenced output using their private key.
+*/
+typedef struct tx_in_s
 {
-	uint8_t     id[SHA256_DIGEST_LENGTH];
-	llist_t     *inputs;
-	llist_t     *outputs;
-} transaction_t;
+	uint8_t     block_hash[SHA256_DIGEST_LENGTH];
+	uint8_t     tx_id[SHA256_DIGEST_LENGTH];
+	uint8_t     tx_out_hash[SHA256_DIGEST_LENGTH];
+	sig_t       sig;
+} tx_in_t, ti_t;
+
+/**
+* struct unspent_tx_out_s - Represents an unspent transaction output
+*
+* Description: This structure identifies transaction outputs that have not yet been 
+* consumed in a transaction input, meaning they are still available for spending.
+*
+* @block_hash: Hash of the block containing the transaction identified by @tx_id
+* @tx_id:      The ID of the transaction containing the unspent output
+* @out:        A copy of the referenced transaction output
+*/
+typedef struct unspent_tx_out_s
+{
+	uint8_t     block_hash[SHA256_DIGEST_LENGTH];
+	uint8_t     tx_id[SHA256_DIGEST_LENGTH];
+	tx_out_t    out;
+} unspent_tx_out_t, uto_t;
+
+/**
+* struct tx_context_s - Tracks the balance available for a specific private key
+* @pub: Public key used to match with a private key
+* @balance: The total available balance for the key
+* @needed: The amount required for sending
+* @tx: The transaction structure
+* @sender: The sender's private key
+* @all_unspent: A list of unspent transaction outputs (uto_t)
+*/
+typedef struct tx_context_s
+{
+	uint8_t       pub[EC_PUB_LEN];
+	int           balance;
+	int           needed;
+	transaction_t *tx;
+	EC_KEY const  *sender;
+	llist_t       *all_unspent;
+} tc_t;
+
+/**
+* struct tx_valid_s - Holds information to validate a transaction
+* @input: The total amount from transaction inputs
+* @output: The total amount from transaction outputs
+* @tx_id: The transaction ID
+* @unspent: A list of unspent transaction outputs (uto_t)
+*/
+typedef struct tx_valid_s
+{
+	uint32_t   input;
+	uint32_t   output;
+	uint8_t    tx_id[SHA256_DIGEST_LENGTH];
+	llist_t    *unspent;
+} tv_t;
+
+/**
+* struct update_list_s - Holds information for updating the list of unspent outputs
+* @hash: The hash of the block containing the transaction
+* @unspent: The list of unspent transaction outputs (uto_t)
+* @tx_id: The transaction ID
+*/
+typedef struct update_list_s
+{
+	uint8_t    hash[SHA256_DIGEST_LENGTH];
+	llist_t    *unspent;
+	uint8_t    tx_id[SHA256_DIGEST_LENGTH];
+} ul_t;
 
 /* Prototypes */
 
-to_t *tx_out_create(uint32_t amount, const uint8_t pub[EC_PUB_LEN]);
+to_t *tx_out_create(
+	uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
+
 
 #endif
