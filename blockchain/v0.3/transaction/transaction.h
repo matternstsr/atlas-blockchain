90adbf4ad960b9492daf1d651767f920f33d494a
matternstsr <matternstsr@gmail.com>
2025-03-10 19:24:43 -0500

    nothing works so started over

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -28,6 +28,21 @@ typedef struct tx_out_s
 	uint8_t     hash[SHA256_DIGEST_LENGTH];
 } tx_out_t, to_t;
 
+/**
+ * struct transaction_s - Transaction structure
+ *
+ * @id:      Transaction ID. A hash of all the inputs and outputs.
+ *           Prevents further alteration of the transaction.
+ * @inputs:  List of `tx_in_t *`. Transaction inputs
+ * @outputs: List of `tx_out_t *`. Transaction outputs
+ */
+typedef struct transaction_s
+{
+	uint8_t     id[SHA256_DIGEST_LENGTH];
+	llist_t     *inputs;
+	llist_t     *outputs;
+} transaction_t;
+
 /* Prototypes */
 
 to_t *tx_out_create(uint32_t amount, const uint8_t pub[EC_PUB_LEN]);
