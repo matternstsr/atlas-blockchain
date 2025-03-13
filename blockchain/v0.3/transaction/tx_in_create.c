9cbdb196e346395e4b181cdd77b88f93dc0cdecc
matternstsr <matternstsr@gmail.com>
2025-03-12 22:37:50 -0500

    blockchain/v0.3/transaction/tx_in_create.c:12: ERROR: parentheses are required on a return statement blockchain/v0.3/transaction/tx_in_create.c:16: WARNING: Missing a blank line after declarations blockchain/v0.3/transaction/tx_in_create.c:17: ERROR: parentheses are required on a return statement blockchain/v0.3/transaction/tx_in_create.c:24: ERROR: parentheses are required on a return statement

--- blockchain/v0.3/transaction/tx_in_create.c
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -9,17 +9,18 @@ ti_t *tx_in_create(const uto_t *unspent)
 {
 	/* Return NULL if the unspent transaction pointer is invalid */
 	if (!unspent)
-		return NULL;
+		return (NULL);
 
 	/* Allocate memory for the new transaction input struct */
 	ti_t *tx_in = calloc(1, sizeof(ti_t));
+
 	if (!tx_in)
-		return NULL;
+		return (NULL);
 
 	/* Copy necessary data from the unspent transaction */
 	memcpy(tx_in->block_hash, unspent->block_hash, SHA256_DIGEST_LENGTH);
 	memcpy(tx_in->tx_id, unspent->tx_id, SHA256_DIGEST_LENGTH);
 	memcpy(tx_in->tx_out_hash, unspent->out.hash, SHA256_DIGEST_LENGTH);
 
-	return tx_in;
+	return (tx_in);
 }
